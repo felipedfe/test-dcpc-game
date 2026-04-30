14e2d6892b46131514b43caf62961f2d6a52a8e2
Felipe Fernandes <felipedfe@gmail.com>
2026-04-30 11:19:39 -0300

    render order baloons

--- js/main.js
+++ js/main.js
@@ -148,6 +148,10 @@ let fundoSprite; // Phaser.GameObjects.Image
 // guarda os gameObjects das crianças
 let containerCriancas;
 
+let containerPratos;
+
+let containerPedidos;
+
 // variável pra guardar os acertos dentro de uma subFase (quando prato é entregue pro personagem certo)
 let acertosNaSubfase;
 
@@ -282,13 +286,23 @@ function fnJogo() {
 }
 
 function aplicarFundoDaFase(faseAtual) {
-  const key = fundoPorFase[faseAtual] ?? 'parede_1';
-  if (!fundoSprite) {
-    fundoSprite = $this.add.image(0, 0, key).setOrigin(0, 0);
-    fundoSprite.setDepth(-5);
-  } else {
-    fundoSprite.setTexture(key);
-  }
+	const key = fundoPorFase[faseAtual] ?? 'parede_1';
+	if (!fundoSprite) {
+		fundoSprite = $this.add.image(0, 0, key).setOrigin(0, 0);
+		fundoSprite.setDepth(-5);
+	} else {
+		fundoSprite.setTexture(key);
+	}
+}
+
+function renderizaPedidos() {
+	containerPedidos = $this.add.container(100, 100);
+	const totalPedidos = 5;
+
+	for (let id = 1; id <= totalPedidos; id += 1) {
+		const balao = $this.add.sprite(0, 0, 'atlas', 'pedido_' + id ).setVisible(false);
+		containerPedidos.add(balao);
+	}
 }
 
 // renderiza sprites das crianças
@@ -329,6 +343,18 @@ function escolheSaborPratoAleatorio() {
 	return SABORES_PRATO[Math.floor(Math.random() * SABORES_PRATO.length)];
 }
 
+function embaralhaOrdemPratos() {
+	const ordem = [1, 2, 3, 4, 5];
+	let i = ordem.length - 1;
+	for (; i > 0; i -= 1) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const t = ordem[i];
+		ordem[i] = ordem[j];
+		ordem[j] = t;
+	}
+	return ordem;
+}
+
 function criaPrato(id, x, y, sabor) {
 	const referenciaPrato = 'prato_' + id + '_' + sabor;
 	const prato = $this.add
@@ -346,16 +372,19 @@ function criaPrato(id, x, y, sabor) {
 }
 
 function renderizaPratos() {
-	const containerPratos = $this.add.container(0, 0);
+	containerPratos = $this.add.container(0, 0);
 
 	let posXDoPrato = 40;
 	const posYLinhaPar = 540;
 	const posYLinhaImpar = 420;
 	const espacoHorizontal = 250;
 	const totalPratos = 5;
+	const ordemIds = embaralhaOrdemPratos();
 
-	for (let id = 1; id <= totalPratos; id += 1) {
-		const posYDoPrato = id % 2 === 0 ? posYLinhaPar : posYLinhaImpar;
+	for (let slot = 0; slot < totalPratos; slot += 1) {
+		const id = ordemIds[slot];
+
+		const posYDoPrato = (slot + 1) % 2 === 0 ? posYLinhaPar : posYLinhaImpar;
 		const sabor = escolheSaborPratoAleatorio();
 		const prato = criaPrato(id, posXDoPrato, posYDoPrato, sabor);
 		containerPratos.add(prato);
@@ -382,19 +411,25 @@ function iniciaJogo() {
 	mesa.setDepth(-3);
 
 	// let containerPratos = renderizaPratos();
-	// containerPratos.setDepth(2);
-	renderizaPratos()
+	renderizaPratos();
 
-	$this.input.on('drag', function(_pointer, gameObject, dragX, dragY) {
+	renderizaPedidos();
+
+	$this.input.on('dragstart', function (_pointer, gameObject) {
+		containerPratos.bringToTop(gameObject);
+	});
+
+	$this.input.on('drag', function (_pointer, gameObject, dragX, dragY) {
 		gameObject.x = dragX;
 		gameObject.y = dragY;
+		// gameObject.setDepth(99);
 	});
 
 	// prato volta pra posição original
-	$this.input.on('dragend', function(_pointer, gameObject) {
-    gameObject.x = gameObject.initialXPos;
-    gameObject.y = gameObject.initialYPos;
-});
+	$this.input.on('dragend', function (_pointer, gameObject) {
+		gameObject.x = gameObject.initialXPos;
+		gameObject.y = gameObject.initialYPos;
+	});
 
 	if (subFase === totalSubFases) {
 		//...
